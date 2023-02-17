4559d945b8269997ffd9e0eb92458343122c46b7
connorbodin <connor.e.bodin@gmail.com>
2023-02-16 19:28:17 -0600

    form added

--- public/js/project.js
+++ public/js/project.js
@@ -1,2 +1,11 @@
+
 const comment = document.getElementById('comment');
 
+commentSection = () => {
+    
+   let form = document.getElementById("comment-form")
+   console.log(form);
+   form.classList.remove("display-none");
+};
+
+comment.addEventListener('click', commentSection);
