8b006cad44840a5c96130aad5e0543e83863a666
connorbodin <connor.e.bodin@gmail.com>
2023-02-24 12:09:03 -0600

    hoping this works :)

--- models/index.js
+++ models/index.js
@@ -12,17 +12,19 @@ User.hasMany(Comment, {
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'user_id'
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
+  foreignKey: 'postId',
   onDelete: 'CASCADE'
-})
+});
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id'
-})
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
 Comment.belongsTo(Post, {
   foreignKey: 'post_id'
 })
